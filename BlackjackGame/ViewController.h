9226077d5754a1d363d109a2305ef171a9aa7868
Cartrette <ccartrette09@ccartrette0m4.local>
2014-05-05 22:12:57 -0500

    ViewController.h and .m edited to add properties and methods

--- BlackjackGame/ViewController.h
+++ BlackjackGame/ViewController.h
@@ -9,5 +9,17 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *HitButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *StandButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *DoubleButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *SplitButton;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *NewDealButton;
+
+- (IBAction)HitCard:(id)sender;
+- (IBAction)StandHand:(id)sender;
+- (IBAction)DoubleHand:(id)sender;
+- (IBAction)SplitHand:(id)sender;
+- (IBAction)NewDeal:(id)sender;
+
 
 @end
