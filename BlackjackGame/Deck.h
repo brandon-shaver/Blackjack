869d8ea732032cfe7c57fd663db91d533490efab
Shaver <bshaver09@bshaver09m4.local>
2014-05-05 22:40:06 -0500

    -Added empty deck class

---
+++ BlackjackGame/Deck.h
@@ -0,0 +1,13 @@
+//
+//  Deck.h
+//  BlackjackGame
+//
+//  Created by Shaver, Brandon L on 5/5/14.
+//  Copyright (c) 2014 Shaver, Brandon L. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Deck : NSObject
+
+@end
---
+++ BlackjackGame/Deck.m
@@ -0,0 +1,13 @@
+//
+//  Deck.m
+//  BlackjackGame
+//
+//  Created by Shaver, Brandon L on 5/5/14.
+//  Copyright (c) 2014 Shaver, Brandon L. All rights reserved.
+//
+
+#import "Deck.h"
+
+@implementation Deck
+
+@end
